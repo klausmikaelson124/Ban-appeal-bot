085fce1a91b9125230df9ea5f57aed4462a54ac3
klausmikaelson124 <181569468+klausmikaelson124@users.noreply.github.com>
2025-01-12 00:25:03 -0800

    Create StartUp.bat

---
+++ StartUp.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting your Ban Appeal Bot...
+cd /d "C:\Users\giffo\OneDrive\Desktop\Hyper Sparks Network\Hyper Sparks Network bot\ban-appeal-bot"  # Path to your bot directory
+npm install                                                             # Install dependencies if needed
+npm start                                                               # Start the bot (or use npm run <your-script-name>)
+pause
